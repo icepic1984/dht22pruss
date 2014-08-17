6d63d1e977fb0a4996db84b2b012ec14e873c1c2
root <root@alarm.base.xx>
2014-08-17 19:50:54 +0200

    Script for query dht22pruss and writing data to rrd database

--- collect.sh
+++ collect.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
+#Data is read every 10 seconds
+#Heartbeat is 20 seconds
+#
+#RRA1: every 10 seconds for 3 days
+#RRA2: every hour for 7 days
+#RRA3: every day for 3 years
 
+ROBINFILE=/mnt/data/temp.rrd
+
+if [ ! -f $ROBINFILE ]
+then
+	echo "Create $ROBINFILE"
+	rrdtool create $ROBINFILE                \
+		--step 10                         \
+		DS:temperature:GAUGE:20:U:U       \
+		DS:humidity:GAUGE:20:U:U          \
+		RRA:AVERAGE:0.5:1:25920           \
+		RRA:AVERAGE:0.5:360:168           \
+		RRA:AVERAGE:0.5:8640:1095         \
+		RRA:MAX:0.5:1:25920               \
+		RRA:MAX:0.5:360:168               \
+		RRA:MAX:0.5:8640:1095             \
+		RRA:MIN:0.5:1:25920               \
+		RRA:MIN:0.5:360:168               \
+		RRA:MIN:0.5:8640:1095
+fi
 
 while true 
 do
-	temperature=`nc alarm 666 | cut -d" " -f2`
-	humidity=$(nc alarm 666 | cut -d" " -f4)
-	echo $temperature
-	echo $humidity
-	sleep 5
+	humidity=`nc alarm 666 | cut -d" " -f2`
+	temperature=$(nc alarm 666 | cut -d" " -f4)
+	rrdtool update $ROBINFILE N:$temperature:$humidity
+	echo "Temp: $temperature Hum: $humidity"
+	sleep 10 
 done
 
